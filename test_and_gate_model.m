3977bf790aea2539bcbdf11347bec61914415f55
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-04-17 14:49:44 +0530

    Update test_and_gate_model.m

--- test_and_gate_model.m
+++ test_and_gate_model.m
@@ -1,33 +1,56 @@
+modelName = 'and_gate_model';
+load_system(modelName);
+
+open_system(modelName);
+
+set_param(modelName, 'SystemTargetFile', 'ert.tlc');
+
+set_param(modelName, 'Toolchain', 'MinGW64 | gmake (64-bit Windows)');
+
+set_param(modelName, 'Solver', 'FixedStepDiscrete');
+
+set_param(modelName, 'FixedStep', '0.1');
+
+rtwbuild(modelName);
+
+checkIDs = {'mathworks.jmaab.ar_0001', 'mathworks.jmaab.ar_0002',
+            'mathworks.jmaab.jc_0241', 'mathworks.jmaab.jc_0242',
+            'mathworks.jmaab.jc_0201', 'mathworks.jmaab.jc_0211',
+            'mathworks.jmaab.jc_0231', 'mathworks.jmaab.jc_0243',
+            'mathworks.jmaab.jc_0247', 'mathworks.jmaab.jc_0244',
+            'mathworks.jmaab.jc_0222', 'mathworks.jmaab.jc_0232',
+            'mathworks.jmaab.jc_0245', 'mathworks.jmaab.jc_0246',
+            'mathworks.jmaab.jc_0795', 'mathworks.jmaab.jc_0796',
+            'mathworks.jmaab.jc_0791', 'mathworks.jmaab.jc_0792',
+            'mathworks.jmaab.jc_0700', 'mathworks.maab.na_0019'};
+
+results = ModelAdvisor.run(modelName, checkIDs);
+
+
 function tests = test_and_gate_model()
     tests = functiontests(localfunctions);
 end
 
 function setupOnce(testCase)
-    % Load the Simulink model
     load_system('and_gate_model.slx');
 end
 
 function teardownOnce(testCase)
-    % No teardown required
 end
 
 
 
 %% Test Functions
 function testAND_TrueTrue(testCase)
-    % Test case: a = 1, b = 1
     set_param('and_gate_model/Constant', 'Value', '1');
     set_param('and_gate_model/Constant1', 'Value', '1');
-    % Run the simulation
     sim('and_gate_model.slx');
-    % Get the output from the Display block
     save matlab.mat;
     load matlab.mat ans;
     a=ans.simout.Data;
     
    % output = get_param('and_gate_model/Display', 'Format');
-    
-    % Assert the output
+
     testCase.verifyEqual(double(a), 1)
     clear ans
 end
@@ -35,33 +58,31 @@ function testAND_TrueFalse(testCase)
     % Test case: a = 1, b = 1
     set_param('and_gate_model/Constant', 'Value', '1');
     set_param('and_gate_model/Constant1', 'Value', '0');
-    % Run the simulation
+
     sim('and_gate_model.slx');
-    % Get the output from the Display block
+
     save matlab.mat;
     load matlab.mat ans;
     a=ans.simout.Data;
     
    % output = get_param('and_gate_model/Display', 'Format');
     
-    % Assert the output
     testCase.verifyEqual(double(a), 0)
     clear ans
 end
+
 function testAND_FalseTrue(testCase)
     % Test case: a = 1, b = 1
     set_param('and_gate_model/Constant', 'Value', '0');
     set_param('and_gate_model/Constant1', 'Value', '1');
-    % Run the simulation
+
     sim('and_gate_model.slx');
-    % Get the output from the Display block
     save matlab.mat;
     load matlab.mat ans;
     a=ans.simout.Data;
     
    % output = get_param('and_gate_model/Display', 'Format');
     
-    % Assert the output
     testCase.verifyEqual(double(a), 0)
     clear ans
 end
@@ -69,16 +90,15 @@ function testAND_FalseFalse(testCase)
     % Test case: a = 1, b = 1
     set_param('and_gate_model/Constant', 'Value', '0');
     set_param('and_gate_model/Constant1', 'Value', '0');
-    % Run the simulation
+
     sim('and_gate_model.slx');
-    % Get the output from the Display block
+
     save matlab.mat;
     load matlab.mat ans;
     a=ans.simout.Data;
     
    % output = get_param('and_gate_model/Display', 'Format');
     
-    % Assert the output
     testCase.verifyEqual(double(a), 0)
     clear ans
 end
